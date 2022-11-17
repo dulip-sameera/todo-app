ed6bb890c818bccb3e23ae2c52635989eb9a88a1
dulip-sameera <dsgitrepo101@gmail.com>
2022-11-17 09:07:04 +0530

    Add page load

---
+++ src/ui/page_load.js
@@ -0,0 +1,49 @@
+import Storage from "../utils/storage.js";
+import createProjectList from "../core/project_list.js";
+
+function createProjectListFromLocalStorage() {
+  const projectList = createProjectList();
+  const storedProjects = Storage.retrieve();
+
+  if (!storedProjects) {
+    return false;
+  }
+
+  for (let i = 0; i < storedProjects.projectNameList.length; i++) {
+    const element = storedProjects.projectNameList[i];
+    projectList.add(element);
+  }
+
+  for (let i = 0; i < projectList.getAll().length; i++) {
+    const singleProject = projectList.getByIndex(i);
+    const todoList = storedProjects.projects[singleProject.getTitle()];
+    for (let j = 0; j < todoList.length; j++) {
+      singleProject.addToDo({
+        title: todoList[j].title,
+        duedate: new Date(todoList[j].duedate),
+        priority: todoList[j].priority,
+        status: todoList[j].status,
+      });
+    }
+  }
+
+  return projectList;
+}
+
+function isProjectListAvailableInLocalStorage() {
+  if (Storage.retrieve()) {
+    return true;
+  }
+  return false;
+}
+
+function pageLoad() {
+  // get a project list if there is one in local storage or create new one
+  const projectList = isProjectListAvailableInLocalStorage()
+    ? createProjectListFromLocalStorage()
+    : createProjectList();
+
+  return projectList;
+}
+
+export { pageLoad };
