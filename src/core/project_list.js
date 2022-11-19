e7e17f456d2235e1de02bce12e1cf6b92c369fcb
dulip-sameera <dsgitrepo101@gmail.com>
2022-11-19 05:56:48 +0530

    Update remove function     now: returns the title of the deleting project     previously: no returns

--- src/core/project_list.js
+++ src/core/project_list.js
@@ -13,7 +13,9 @@ export default function createProjectList() {
   };
 
   const remove = (index) => {
+    const title = getByIndex(index).getTitle();
     _list.splice(index, 1);
+    return title;
   };
 
   const getByIndex = (index) => {
