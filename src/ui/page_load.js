1bc21dd1109480e85a90d8d83e71db57051a1dbf
dulip-sameera <dsgitrepo101@gmail.com>
2022-11-19 05:59:04 +0530

    Update page_load     remove dead code     Update pageLoad function         now: it generate projectList in sidebar         previously: returns projectList

--- src/ui/page_load.js
+++ src/ui/page_load.js
@@ -1,5 +1,6 @@
 import Storage from "../utils/storage.js";
 import createProjectList from "../core/project_list.js";
+import generateProject from "./projects.js";
 
 function createProjectListFromLocalStorage() {
   const projectList = createProjectList();
@@ -11,12 +12,14 @@ function createProjectListFromLocalStorage() {
 
   for (let i = 0; i < storedProjects.projectNameList.length; i++) {
     const element = storedProjects.projectNameList[i];
+    // console.log(element);
     projectList.add(element);
   }
 
   for (let i = 0; i < projectList.getAll().length; i++) {
     const singleProject = projectList.getByIndex(i);
     const todoList = storedProjects.projects[singleProject.getTitle()];
+    // console.log(todoList);
     for (let j = 0; j < todoList.length; j++) {
       singleProject.addToDo({
         title: todoList[j].title,
@@ -43,7 +46,7 @@ function pageLoad() {
     ? createProjectListFromLocalStorage()
     : createProjectList();
 
-  return projectList;
+  generateProject(projectList);
 }
 
 export { pageLoad };
